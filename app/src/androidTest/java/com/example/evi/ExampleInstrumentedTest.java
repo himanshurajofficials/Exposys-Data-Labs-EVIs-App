c6992308aa998850a3f1c37f6861ab5ce632e8d1
Himanshu Raj <81401794+masterHimanshu@users.noreply.github.com>
2022-10-27 12:09:13 +0530

    Update ExampleInstrumentedTest.java
    Co-Authored-By: wildshark2 <116789245+wildshark2@users.noreply.github.com>

--- app/src/androidTest/java/com/example/evi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/evi/ExampleInstrumentedTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ *this is an android app
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
